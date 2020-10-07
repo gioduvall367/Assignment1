dcc9843517df2796c23a4fe748cddde49aaa00a7
gioduvall367 <gioduvall@gmail.com>
2020-10-06 21:58:25 -0700

    v1

---
+++ src/cse360assignment02/AddingMachine.java
@@ -0,0 +1,32 @@
+package cse360assignment02;
+
+public class AddingMachine {
+    private int total;
+
+    // constructor for the AddingMachine class
+    public AddingMachine () {
+        total = 0;  // not needed - included for clarity
+    }
+
+    // Returns the total of what has been added up
+    public int getTotal () {
+        return 0;
+    }
+
+    // Performs the add
+    public void add (int value) {
+    }
+
+    // Subtracts the parameter from the value
+    public void subtract (int value) {
+    }
+
+    // Converts to a string value
+    public String toString () {
+        return "";
+    }
+
+
+    public void clear() {
+    }
+}
